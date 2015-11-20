f12542a03ac05e3b84bbbbebd352c57fc93e615d
Matt Popovich <mattpopovich@outlook.com>
2015-11-20 11:02:08 -0500

    Removed corrupted migration file

--- db/migrate/20151119163222_create_blaaaargh.rb
+++
@@ -1,9 +0,0 @@
-class CreateFollowings < ActiveRecord::Migration
-  def change
-    create_table :followings do |t|
-      t.integer :user_id
-      t.integer :follower_id
-      t.timestamps null: false
-    end
-  end
-end
